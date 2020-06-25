e29a451d76b469b40ee9b2548f01f270d051aa10
chaks03 <chakshu@samagragovernance.in>
2020-06-25 09:31:57 +0530

    Iniital commit

---
+++ src/test/java/com/samagra/orchestrator/IntegrationTest.java
@@ -0,0 +1,34 @@
+package com.samagra.orchestrator;
+
+import com.samagra.orchestrator.Drools.DroolsBeanFactory;
+import messagerosa.core.model.XMessage;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.api.io.Resource;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+
+public class IntegrationTest {
+    private KieSession kSession;
+
+    @Before
+    public void setup() {
+        Resource resource = ResourceFactory.newClassPathResource("com/samagra/orchestrator/Drools/OrchestratorRules.xlsx", getClass());
+        kSession = new DroolsBeanFactory().getKieSession(resource);
+        System.out.println(new DroolsBeanFactory().getDrlFromExcel("com/samagra/orchestrator/Drools/OrchestratorRules.xlsx"));
+    }
+
+    @Test
+    public void testChangeInChannel() throws Exception {
+        XMessage xMessage = new XMessage();
+        xMessage.setMessageState(XMessage.MessageState.DELIVERED);
+        xMessage.setChannel("WhatsApp");
+        xMessage.setTimestamp("1592900000");
+        System.out.println(xMessage.secondsSinceLastMessage());
+        kSession.setGlobal("xMessage", xMessage);
+        kSession.insert(xMessage);
+        kSession.fireAllRules();
+        System.out.println(xMessage.toXML());
+    }
+
+}
