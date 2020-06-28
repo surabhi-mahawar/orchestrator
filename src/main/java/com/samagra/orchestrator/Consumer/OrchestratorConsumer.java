6c7609953647225062ae5d690f37e91115ac8a15
chaks03 <chakshu@samagragovernance.in>
2020-06-28 14:11:32 +0530

    Feature: testing

--- src/main/java/com/samagra/orchestrator/Consumer/OrchestratorConsumer.java
+++ src/main/java/com/samagra/orchestrator/Consumer/OrchestratorConsumer.java
@@ -1,6 +1,9 @@
 package com.samagra.orchestrator.Consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import messagerosa.core.model.Transformer;
+import messagerosa.core.model.TransformerRegistry;
+import messagerosa.xml.XMessageParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -11,13 +14,48 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import lombok.extern.slf4j.Slf4j;
+import messagerosa.core.model.XMessage;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 
 @Slf4j
 @Service
 public class OrchestratorConsumer {
 
-    @KafkaListener(id = "orchestrator", topics = "${gupshup-incoming-message}")
+    @KafkaListener(id = "orchestrator", topics = "${inbound-processed}")
     public void consumeMessage(String message) throws Exception {
         System.out.println(message);
+
+        XMessage msg = XMessageParser.parse(new ByteArrayInputStream(message.getBytes()));
+
+        // Assign Transformer
+        ArrayList<Transformer> transformers = new ArrayList<>();
+        transformers.add(new Transformer("3")); //Forms
+        transformers.add(new Transformer("4")); //Outbound
+        msg.setTransformers(transformers); //TODO change this to Drools based
+        String transformer = TransformerRegistry.getName(msg.getTransformers().get(0).getId());
+
+        // Send message to "transformer" topic => "Forms"
+
+
+    }
+
+    @KafkaListener(id = "orchestrator", topics = "${transformer-processed}")
+    public void consumeMessageTransformer(String message) throws Exception {
+        System.out.println(message);
+
+        XMessage msg = XMessageParser.parse(new ByteArrayInputStream(message.getBytes()));
+
+        // Assign Transformer
+        ArrayList<Transformer> transformers = new ArrayList<>();
+        transformers.add(new Transformer("3")); //Forms
+        transformers.add(new Transformer("4")); //Outbound
+        msg.setTransformers(transformers); //TODO change this to Drools based
+        String transformer = TransformerRegistry.getName(msg.getTransformers().get(0).getId());
+
+        // Send message to "transformer" topic => "Forms"
+
+
     }
 }
