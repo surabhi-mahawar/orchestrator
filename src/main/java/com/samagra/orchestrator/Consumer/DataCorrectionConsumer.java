486883bcc07f8aedc1e584eecc07f70062e0a62d
Vishal Singla <vishal.singla@africa.airtel.com>
2020-08-06 23:27:19 +0530

    data fix

---
+++ src/main/java/com/samagra/orchestrator/Consumer/DataCorrectionConsumer.java
@@ -0,0 +1,24 @@
+package com.samagra.orchestrator.Consumer;
+
+import com.samagra.orchestrator.Publisher.CommonProducer;
+import messagerosa.core.model.XMessage;
+import messagerosa.dao.XMessageDAO;
+import messagerosa.dao.XMessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class DataCorrectionConsumer {
+    @Autowired
+    public XMessageRepo xmsgRepo;
+
+    @KafkaListener(id = "datacorrection", topics = "${inboundProcessed}")
+    public void consumeMessage(XMessage message) throws Exception {
+        if(message.getMessageState().equals(XMessage.MessageState.REPLIED)){
+            String gsmsid = message.getMessageId().getGupshupMessageId();
+            XMessageDAO msgDao = xmsgRepo.findByGupShupMessageId(gsmsid);
+            String xmsgText = message.getPayload().getText();
+            msgDao.setXMessage(xmsgText);
+            xmsgRepo.save(msgDao);
+        }
+    }
+}
