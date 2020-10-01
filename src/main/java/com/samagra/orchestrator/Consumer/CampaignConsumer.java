8ceef1d616bb41631a6a376b4abe124f1034aa4f
chaks03 <chakshu@samagragovernance.in>
2020-10-02 00:14:14 +0530

    Fix: CDAC Broadcast update

--- src/main/java/com/samagra/orchestrator/Consumer/CampaignConsumer.java
+++ src/main/java/com/samagra/orchestrator/Consumer/CampaignConsumer.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -25,7 +26,7 @@ public class CampaignConsumer {
     public void consumeMessage(String campaignID) throws Exception {
         XMessage xMessage = processMessage(campaignID);
         log.info("Pushing to : "+ TransformerRegistry.getName(xMessage.getTransformers().get(0).getId()));
-        kafkaProducer.send("CampaignMessageMultiplexer", xMessage.toXML());
+        kafkaProducer.send("BroadcastTransformer", xMessage.toXML());
     }
 
     /**
@@ -37,37 +38,40 @@ public class CampaignConsumer {
     public static XMessage processMessage(String campaignID) throws Exception {
         // Get campaign ID and get campaign details {data: transformers [broadcast(SMS), <formID>(Whatsapp)]}
         Application campaignDetails = CampaignService.getCampaignFromID(campaignID);
-        ArrayList<String> transformerDetails = (ArrayList) campaignDetails.data.get("transformers");
-        // Create a new campaign xMessage
-        String channelURI = "";
-        String transformerID = "";
-        String key = "";
-        String value = "";
-        if (transformerDetails.get(0).contains(SMS_BROADCAST_IDENTIFIER)) {
-            channelURI = "SMS";
-            key = "Template";
-            value = transformerDetails.get(0).split("::")[1];
-            transformerID = "1";
-        } else {
-            channelURI = "WhatsApp";
-            transformerID = "2";
-            key = "Form";
-            value = transformerDetails.get(0).split("::")[1];
-        }
+        ArrayList<HashMap<String, String>> transformerDetails = (ArrayList) campaignDetails.data.get("parts");
 
-        HashMap<String, String> hashMap = new HashMap();
-        hashMap.put(key, value);
-        Transformer transformer = new Transformer(transformerID, hashMap);
+        // Create a new campaign xMessage
+        XMessagePayload payload = XMessagePayload.builder()
+                .text(transformerDetails.get(0).get("msg"))
+                .build();
+
+        SenderReceiverInfo to = SenderReceiverInfo.builder()
+                .userID((String) campaignDetails.data.get("group"))
+                .build();
+
+        Transformer broadcast = Transformer.builder()
+                .id("1")
+                .build();
         ArrayList<Transformer> transformers = new ArrayList<>();
-        transformers.add(0, transformer);
-        new XMessage();
+        transformers.add(broadcast);
+
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("senderID", "HPGOVT");
+        SenderReceiverInfo from = SenderReceiverInfo.builder()
+                .userID("hpgovt-hpssa")
+                .meta(metadata)
+                .build();
+
         return XMessage.builder()
                 .app(campaignDetails.name)
-                .channelURI(channelURI)
+                .channelURI(transformerDetails.get(0).get("channel"))
+                .providerURI(transformerDetails.get(0).get("provider"))
+                .payload(payload)
                 .conversationStage(new ConversationStage(0, ConversationStage.State.STARTING))
-                .providerURI("Gupshup")
                 .timestamp(System.currentTimeMillis())
                 .transformers(transformers)
+                .to(to)
+                .from(from)
                 .build();
     }
 }
--- src/main/java/com/samagra/orchestrator/User/CampaignService.java
+++ src/main/java/com/samagra/orchestrator/User/CampaignService.java
@@ -4,10 +4,12 @@ import com.inversoft.rest.ClientResponse;
 import io.fusionauth.client.FusionAuthClient;
 import io.fusionauth.domain.Application;
 import io.fusionauth.domain.api.ApplicationResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 
+@Slf4j
 @Component
 public class CampaignService {
 
@@ -18,27 +20,16 @@ public class CampaignService {
      * @throws Exception Error Exception, in failure in Network request.
      */
     public static Application getCampaignFromID(String campaignID) throws Exception {
-        System.out.println("CampaignID: " + campaignID);
         FusionAuthClient staticClient = new FusionAuthClient("c0VY85LRCYnsk64xrjdXNVFFJ3ziTJ91r08Cm0Pcjbc", "http://134.209.150.161:9011");
-        System.out.println("Client: " + staticClient);
         ClientResponse<ApplicationResponse, Void> applicationResponse = staticClient.retrieveApplication(UUID.fromString(campaignID));
+        Application application = null;
         if(applicationResponse.wasSuccessful() ) {
-            Application application = applicationResponse.successResponse.application;
-            Map<String,Object> campaignData = new HashMap<>();
-            ArrayList<String> transformers = new ArrayList<>();
-            // transformers.add(0, "Broadcast::SMS_1"); //SMS_1 refers to the template ID.
-            // transformers.add(1, "FORM::FORM_ID_1"); //Form_ID_1 refers to first step ODK Form
-            transformers.add(0, "FORM::RQ"); //Form_ID_2 refers to second step ODK Form
-            //If it contains only Broadcast ---> SMS based campaign
-            //If only FORM::Form_ID --> ODK Based campaign
-            campaignData.put("transformers", transformers);
-            application.data = campaignData;
-            return application;
+             application = applicationResponse.successResponse.application;
         }else if (applicationResponse.exception != null) {
             Exception exception = applicationResponse.exception;
-            throw  exception;
+            log.error("Campaign not found with this ID ::" + exception.getMessage());
         }
-        return null;
+        return application;
     }
 
     /**
